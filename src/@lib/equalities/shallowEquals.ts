9446b7f70f45c384e7277c8ec7e846d071393c02
kutta97 <yhjin04170@gmail.com>
2025-04-09 00:19:38 +0900

    feat: shallowEquals 함수 비교 로직 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,26 @@
+function isObject(value: unknown): value is Record<string, unknown> {
+  return value !== null && typeof value === "object";
+}
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((_, i) => objA[i] === objB[i]);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => objA[key] === objB[key]);
+  }
+
   return objA === objB;
 }
